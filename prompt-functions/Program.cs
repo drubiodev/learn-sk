62c242d849115810780c7eecf1f13d663937cc64
Daniel Rubio <drubio432@gmail.com>
2024-02-19 19:47:50 -0500

    implementation of prompt function

--- prompt-functions/Program.cs
+++ prompt-functions/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 Console.WriteLine("Prompt Functions");
 
@@ -7,3 +9,34 @@ var builder = new ConfigurationBuilder()
             .AddJsonFile("local.appsettings.json", optional: true, reloadOnChange: true);
 
 IConfigurationRoot configuration = builder.Build();
+
+Console.WriteLine("Enter the text with city information");
+
+var userInput = Console.ReadLine();
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(configuration["MODEL_AI"], configuration["ENDPOINT"], configuration["API_KEY"])
+    .Build();
+
+//Import Prompt Functions
+var plugInDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins", "location");
+kernel.ImportPluginFromPromptDirectory(plugInDirectory, "location");
+
+//Get the function from location plugin
+var cityKernelFunction = kernel.Plugins.GetFunction("location", "city");
+
+//Kernel arguments
+
+var kernelArguments = new KernelArguments()
+    {
+        {
+            "input", userInput
+        }
+    };
+
+
+var functionResults = await kernel.InvokeAsync(cityKernelFunction, kernelArguments);
+
+Console.WriteLine(functionResults.GetValue<string>());
+
+Console.Read();
